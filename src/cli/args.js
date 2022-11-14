97265439503a39e96a4b7e10a2e674d46f85b1fe
Aliaksei Kasiuk <aliakseikasiuk@gmail.com>
2022-11-14 14:47:41 +0300

    init: start 'Node.js basics' task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const propName1 = args[0].slice(2);
+  const propName2 = args[2].slice(2);
+  const value1 = args[1];
+  const value2 = args[3];
+
+  const result = `${propName1} is ${value1}, ${propName2} is ${value2}`;
+  console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
 const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_asf = 'sdfds';
+  process.env.RSS_sdfgsfd = 'dfg';
+  process.env.RSS_gfdhdfghdfgd = 'dfghdg33';
+
+  const variables = process.env;
+  let rssVariables = [];
+
+  for (const variable in variables) {
+    if (variable.indexOf('RSS_') == -1) continue;
+
+    rssVariables.push(`${variable}=${variables[variable]}`);
+  }
+
+  const result = rssVariables.join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
