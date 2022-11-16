c929c92de4743cb2feefb5edad9d3ad6c57f62e0
Aliaksei Kasiuk <aliakseikasiuk@gmail.com>
2022-11-16 13:24:12 +0300

    feat: complete 'hash' subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+const { createHmac } = await import('node:crypto');
+import { readFile } from 'node:fs/promises';
+import { setPath } from '../helper/setPath.js';
+
+const filePath = setPath(import.meta.url, 'files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const text = await readFile(filePath);
+  const hash = createHmac('sha256', text).update(text).digest('hex');
+
+  console.log(hash);
 };
 
 await calculateHash();
